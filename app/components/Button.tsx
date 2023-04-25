594ecc37a714d7dad40a7ef58cfe6ec64979a01d
Sanjay <sanu2336@gmail.com>
2023-04-25 06:59:29 +0530

    initial commit

---
+++ app/components/Button.tsx
@@ -0,0 +1,15 @@
+import { DetailedHTMLProps, ButtonHTMLAttributes, forwardRef } from "react";
+
+export const Button = forwardRef<
+  HTMLButtonElement,
+  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+>(({ className, children, ...rest }, ref) => {
+  return (
+    <button
+      className={`text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ${className}`}
+      {...rest}
+      ref={ref}>
+      {children}
+    </button>
+  );
+});
